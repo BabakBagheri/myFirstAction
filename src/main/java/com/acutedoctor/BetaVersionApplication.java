ce6a79a9c370da84cd4e8c73a865110eef9ee708
babak-bagheri-wcar <babak.bagheri@wireless.com>
2022-12-14 13:27:03 +0100

    text

--- src/main/java/com/acutedoctor/BetaVersionApplication.java
+++ src/main/java/com/acutedoctor/BetaVersionApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BetaVersionApplication {
 
 	public static void main(String[] args) {
+
+		
 		SpringApplication.run(BetaVersionApplication.class, args);
 	}
 
